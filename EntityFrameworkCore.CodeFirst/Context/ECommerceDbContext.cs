1c3c77da7c8a883eca1547b853ed3be6668a84ff
eaktas <eaktas@innova.com.tr>
2022-09-18 20:17:27 +0300

    Code First

---
+++ EntityFrameworkCore.CodeFirst/Context/ECommerceDbContext.cs
@@ -0,0 +1,22 @@
+﻿using EntityFrameworkCore.CodeFirst.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.CodeFirst.Context
+{
+    public class ECommerceDbContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"Server=N303434\SQLEXPRESS;Database=ECommerce;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ EntityFrameworkCore.CodeFirst/Program.cs
@@ -0,0 +1,23 @@
+﻿using EntityFrameworkCore.CodeFirst.Context;
+using EntityFrameworkCore.CodeFirst.Entities;
+using System;
+using System.Linq;
+
+namespace EntityFrameworkCore.CodeFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ECommerceDbContext northwindContext = new ECommerceDbContext())
+            {
+                var customers = northwindContext.Customers.ToList();
+                foreach (Customer customer in customers)
+                {
+                    Console.WriteLine($"{customer.Name} {customer.Name}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
