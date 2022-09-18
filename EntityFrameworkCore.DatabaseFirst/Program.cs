04c033e8fcfc5132de44f7b9ec83cad69ab72014
eaktas <eaktas@innova.com.tr>
2022-09-18 18:57:35 +0300

    Database First

--- EntityFrameworkCore.DatabaseFirst/Program.cs
+++ EntityFrameworkCore.DatabaseFirst/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿
+using EntityFrameworkCore.DatabaseFirst.Context;
+using EntityFrameworkCore.DatabaseFirst.Entities;
+using System;
+using System.Linq;
 
 namespace EntityFrameworkCore.DatabaseFirst
 {
@@ -6,7 +10,15 @@ namespace EntityFrameworkCore.DatabaseFirst
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (NorthwindContext northwindContext = new NorthwindContext())
+            {
+                var products = northwindContext.Products.ToList();
+                foreach (Product product in products)
+                {
+                    Console.WriteLine($"{product.ProductName}");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
